e05ea30da926dd605704c6f6849f3ad13c729e7e
zhangqian <zhangqian@sdpjw.com>
2024-04-17 18:10:27 +0800

    feat:引入模型demo

--- src/components/vr-wander/index.js
+++ src/components/vr-wander/index.js
@@ -2,7 +2,7 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import CameraControls from 'camera-controls'
-
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 CameraControls.install({ THREE: THREE })
 export class VRWander {
     /**
@@ -11,6 +11,11 @@ export class VRWander {
     _options = {
         // 容器
         container: document.body,
+        // 相机配置
+        cameraOption: {
+            position: { x: 0, y: 2, z: 0 },
+            lookAt: { x: 2, y: 2, z: 2 },
+        },
     }
 
     _scene = null // 场景
@@ -25,14 +30,31 @@ export class VRWander {
         height: window.innerHeight
     }
 
+    // 相机和视点的距离
+    _EPS = 1e-5;
+    // 展厅模型
+    _hallMesh = null;
+    // 展厅地板名称
+    _hallPlaneName = "plane";
+    _planeMesh = null;
+
     constructor(options) {
-        Object.assign(this._options,options)
+        Object.assign(this._options, options)
         this._size.width = this._options.container.clientWidth
         this._size.height = this._options.container.clientHeight
 
         this._init()
+        this._lookat().then(() => {
+            // if (this._options.callback) {
+            //   this._options.callback();
+            // }
+        });
         this._animate()
         window.addEventListener("resize", this._resize.bind(this));
+        this._controls.addEventListener('change', () => {
+            // 浏览器控制台查看相机位置变化
+            console.log('camera.position', this._camera.position);
+        });
     }
     /**
      * 初始化
@@ -41,19 +63,22 @@ export class VRWander {
         // 创建场景
         this._scene = new THREE.Scene()
         // 创建相机
-        this._camera = new THREE.PerspectiveCamera(60, this._size.width / this._size.height, 0.1, 1000)
+        const { width, height } = this._size
+        this._camera = new THREE.PerspectiveCamera(70, width / height, 0.1, 1000)
+        this._camera.position.set(3, 1, 0)
+        // this._camera.lookAt(0, 0, 0)
         // 将相机添加到场景中
         this._scene.add(this._camera)
         // 创建渲染器
         this._renderer = new THREE.WebGLRenderer({
             canvas: this._options.container,
+            alpha: true,
             antialias: true,// 是否执行抗锯齿
             transparent: true,
+            logarithmicDepthBuffer: true
         })
         this._resizeRendererToDisplaySize()
-
-        // 相机控制器
-        this._controls = new CameraControls(this._camera, this._renderer.domElement)
+        this._renderer.sortObjects = true
 
         // 环境光
         this._scene.add(new THREE.AmbientLight(0xffffff, 1));
@@ -61,13 +86,47 @@ export class VRWander {
         const directionLight = new THREE.DirectionalLight(0xffffff, 0.7);
         directionLight.position.set(5, 5, 5);
         this._scene.add(directionLight);
+
+        // this._controls = new OrbitControls(this._camera, this._renderer.domElement);
+        // this._controls.target.set(-2, 1, 0)
+        // this._controls.update()
+        // this._controls.enablePan = false;
+        // this._controls.enableZoom = true;
+
+        // 坐标轴辅助对象
+        const axesHelper = new THREE.AxesHelper(10)
+        this._scene.add(axesHelper)
+
+        // const helper = new THREE.CameraHelper(this._camera);
+        // this._scene.add(helper);
+
+        // 相机控制器
+        this._controls = new CameraControls(this._camera, this._renderer.domElement)
+
+        // this._controls.maxDistance = this._EPS;
+        // this._controls.minZoom = 0.5;
+        // this._controls.maxZoom = 5;
+        // this._controls.dragToOffset = false;
+        // this._controls.distance = 1;
+        // this._controls.dampingFactor = 0.01; // 阻尼运动
+        this._controls.truckSpeed = 0.01; // 拖动速度
+        this._controls.mouseButtons.wheel = CameraControls.ACTION.ZOOM;
+        this._controls.mouseButtons.right = CameraControls.ACTION.NONE;
+        this._controls.touches.two = CameraControls.ACTION.TOUCH_ZOOM;
+        this._controls.touches.three = CameraControls.ACTION.NONE;
+
+        // // 逆向控制
+        // this._controls.azimuthRotateSpeed = -0.5; // 方位角旋转速度。
+        // this._controls.polarRotateSpeed = -0.5; // 极旋转的速度。
+        // this._controls.saveState();
     }
     /**
    * 执行渲染及动画
    */
     _animate() {
         if (this._renderer) {
-        this._renderer.render(this._scene, this._camera);
+            this._renderer.render(this._scene, this._camera);
+            // this._controls.update()
         }
 
         if (this._animates) {
@@ -77,6 +136,28 @@ export class VRWander {
         }
 
         requestAnimationFrame(this._animate.bind(this));
+    }
+
+    async _lookat() {
+        if (!this._options.cameraOption) {
+            return;
+        }
+        const { position, lookAt } = this._options.cameraOption;
+        const lookatV3 = new THREE.Vector3(position.x, position.y, position.z);
+        lookatV3.lerp(new THREE.Vector3(lookAt.x, lookAt.y, lookAt.z), this._EPS);
+        this._controls.zoomTo(0.8);
+        // this._controls.setTatget(-2, 1, 0)
+        // this._controls.update()
+        await this._controls.setLookAt(
+            position.x,
+            position.y,
+            position.z,
+            lookatV3.x,
+            lookatV3.y,
+            lookatV3.z,
+            false
+        );
+        // this._controls.update()
     }
     /**
      * @desc gltf加载器
@@ -89,56 +170,35 @@ export class VRWander {
                 url,
                 position,
                 scale = 1,
-                rotation,
                 onProgress,
-                animate,
-                autoLight, // 自动增亮
             } = params;
             this._gltfLoader.load(url, (gltf) => {
                 const mesh = gltf.scene;
-                const box = new THREE.Box3()
-                .setFromObject(mesh)
-                .getSize(new THREE.Vector3());
-                console.log("box模型大小", url, box, mesh);
+                // const box = new THREE.Box3()
+                //     .setFromObject(mesh)
+                //     .getSize(new THREE.Vector3());
+                // console.log("box模型大小", url, box, mesh);
 
-                if (autoLight) {
-                    gltf.scene.traverse((child) => {
-                        if (child.isMesh) {
-                        // child.frustumCulled = false;
-                        //模型阴影，开启阴影比较消耗性能
-                        child.castShadow = true;
-                        //模型自发光
-                        child.material.emissive = child.material.color;
-                        child.material.emissiveMap = child.material.map;
-                        }
-                    });
-                }
-
-                mesh.scale.set(scale, scale, scale);
+                mesh.scale.set(scale * 0.05, scale * 0.05, scale * 0.05);
                 if (position) {
-                    mesh.position.y = position.y;
-                    mesh.position.x = position.x;
-                    mesh.position.z = position.z;
+                    // mesh.position.y = position.y;
+                    // mesh.position.x = position.x;
+                    // mesh.position.z = position.z;
+                    mesh.position.set(position.x, position.y, position.z)
                 }
-                if (rotation) {
-                    mesh.rotation.y = rotation.y;
-                    mesh.rotation.x = rotation.x;
-                    mesh.rotation.z = rotation.z;
-                }
+
                 this._scene.add(mesh);
-                if (animate) {
-                    mesh.animations = animate;
-                }
+
                 resolve(gltf);
             },
-            (progress) => {
-                if (onProgress) {
-                    onProgress(progress);
+                (progress) => {
+                    if (onProgress) {
+                        onProgress(progress);
+                    }
+                },
+                (err) => {
+                    console.error(err);
                 }
-            },
-            (err) => {
-                console.error(err);
-            }
             );
         });
     }
@@ -147,9 +207,9 @@ export class VRWander {
         return await this.loadGLTF({ ...params }).then((gltf) => {
             this._hallMesh = gltf.scene;
             gltf.scene.traverse((mesh) => {
-            if (mesh.name === params.planeName) {
-                this._planeMesh = mesh;
-            }
+                if (mesh.name === params.planeName) {
+                    this._planeMesh = mesh;
+                }
             });
             return gltf;
         });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,26 +9,29 @@ import { VRWander } from "@/components/vr-wander";
 onMounted(() => {
   const vrWander = new VRWander({
     container: document.getElementById("wanderContainer"),
+    cameraOption: {
+      position: { x: 16.928, y: 2, z: 0.699 },
+      lookAt: { x: 30.551, y: 2, z: 1.096 },
+    },
   });
-  vrWander
-    .loadGLTF({
-      url: "./models/room2/dm.glb",
-      planeName: "dm", // plane , meishu01
-      // position: { x: 2, y: -0.2, z: 2 },
-      position: { x: 0, y: 0, z: 0 },
-      scale: 1,
-      onProgress: (p) => {
-        console.log("加载进度", p);
-      },
-    })
-    .then((gltf) => {
-      console.log(gltf, "gltf");
-    });
+
+  vrWander.loadHall({
+    url: "/models/village/scene.gltf",
+    position: { x: 0, y: 0, z: 0 },
+    scale: 1,
+    onProgress: (p) => {
+      console.log("加载进度", p);
+    },
+  }).then(gltf=>{
+    console.log(gltf,'gltf')
+  })
 });
 </script>
 <style>
 #wanderContainer {
   width: 100%;
   height: 100%;
+  background:url('/bg-village.png') no-repeat center center;
+  background-size:100% 100%;
 }
 </style>
